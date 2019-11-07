49db050469d5f1e72f29640f80801540a824be39
komugirice <iys60111118082@gmail.com>
2019-11-07 14:50:51 +0900

    SearchView追加

--- app/src/main/java/com/example/qiitaapplication/activity/MainActivity.kt
+++ app/src/main/java/com/example/qiitaapplication/activity/MainActivity.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import android.view.Menu
 import android.view.MenuItem
 import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.widget.SearchView
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.FragmentManager
 import androidx.fragment.app.FragmentPagerAdapter
@@ -11,6 +12,8 @@ import com.example.qiitaapplication.R
 import com.example.qiitaapplication.fragment.ArticleFragment
 import com.example.qiitaapplication.fragment.FavoriteFragment
 import kotlinx.android.synthetic.main.activity_main.*
+
+
 
 
 /**
@@ -27,7 +30,7 @@ class MainActivity : AppCompatActivity() {
      */
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(com.example.qiitaapplication.R.layout.activity_main)
 
         initialize()
     }
@@ -49,6 +52,7 @@ class MainActivity : AppCompatActivity() {
         initTabLayout()
     }
 
+
     /**
      * initToolbarメソッド
      *
@@ -59,20 +63,44 @@ class MainActivity : AppCompatActivity() {
         supportActionBar?.setDisplayShowTitleEnabled(false)
     }
 
+
     /**
      * onCreateOptionsMenuメソッド
+     * menuのメソッド
      *
      * @param menu
      */
     override fun onCreateOptionsMenu(menu: Menu) : Boolean {
         // オプションメニュー用xmlファイルをインフレイト
-        menuInflater.inflate(R.menu.menu_options_menu_list, menu)
+        menuInflater.inflate(com.example.qiitaapplication.R.menu.menu_options_menu_list, menu)
+
+        //=== searchView設定 ===
+        val searchItem = menu.findItem(com.example.qiitaapplication.R.id.searchView) as MenuItem
+        val searchView = searchItem.getActionView() as SearchView
+
+        // SearchViewに何も入力していない時のテキストを設定
+        searchView.setQueryHint(this.getResources().getString(R.string.label_search_title))
+
+        // イベントリスナ設定
+        searchView.setOnQueryTextListener(object : SearchView.OnQueryTextListener {
+            override fun onQueryTextSubmit(query: String?): Boolean {
+                return false;
+            }
+
+            override fun onQueryTextChange(newText: String?): Boolean {
+                return true;
+            }
+
+        })
+
+
         // 親クラスの同名メソッドを呼び出し、その戻り値を返却
         return super.onCreateOptionsMenu(menu)
     }
 
     /**
      * onOptionsItemSelectedメソッド
+     * menuのメソッド
      *
      * @param item
      */
@@ -80,7 +108,7 @@ class MainActivity : AppCompatActivity() {
 
         when(item.itemId) {
             // 終了
-            R.id.menuListOptionsFinish -> finish()
+            com.example.qiitaapplication.R.id.menuListOptionsFinish -> finish()
         }
 
         return super.onOptionsItemSelected(item)
